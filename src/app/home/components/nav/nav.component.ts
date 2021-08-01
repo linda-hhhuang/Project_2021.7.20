f95a6f173b60b68e2036e5252d5c8ec7783e322d
huangld <1030545886@qq.com>
2021-08-01 15:21:33 +0800

    真的完成账户管理

--- src/app/home/components/nav/nav.component.ts
+++ src/app/home/components/nav/nav.component.ts
@@ -3,31 +3,58 @@ import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { UserService } from '@core/service/user.service';
 import { Router } from '@angular/router';
+import { User } from '@core/model/user';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  user: any;
+  user!: User;
   role: any = {
     0: '超管',
     1: '教务',
     2: '教师',
     3: '学生',
   };
+  isVisibleResetPassword = false;
+  isOkLoadingResetPassword = false;
+  resetPasswordValue: string = '';
+
   constructor(
     private message: NzMessageService,
-    private userServ: UserService,
+    private userSrvc: UserService,
     private readonly router: Router
   ) {
-    this.userServ.user$.subscribe((v) => (this.user = v));
+    this.userSrvc.user$.subscribe((v) => (this.user = v));
   }
+
   logout() {
-    this.userServ.logout().subscribe(() => {
+    this.userSrvc.logout().subscribe(() => {
       this.message.success(`成功登出！`);
       this.router.navigateByUrl('/login');
     });
   }
+
+  showModalResetPassword() {
+    this.isVisibleResetPassword = true;
+  }
+
+  handleOkResetPassword(): void {
+    this.isOkLoadingResetPassword = true;
+    this.userSrvc.resetPassword(this.resetPasswordValue).subscribe((_) => {
+      this.message.success(`成功修改用户密码为 ${this.resetPasswordValue}`);
+      this.resetPasswordValue = '';
+      this.router.navigateByUrl('/login');
+    });
+    this.isOkLoadingResetPassword = false;
+    this.isVisibleResetPassword = false;
+  }
+
+  handleCancelResetPassword(): void {
+    this.isVisibleResetPassword = false;
+    this.resetPasswordValue = '';
+  }
+
   ngOnInit(): void {}
 }
